8614ca4fd989531a66c48ab9bda96cf7379d06bc
Uglypr1nces <shpatavdiu4@gmail.com>
2024-08-19 19:35:57 +0200

    added url params to get missing variables

--- birthdaycard/script.js
+++ birthdaycard/script.js
@@ -3,30 +3,32 @@ let recipient;
 let text;
 let audio_link;
 
-
 document.addEventListener("DOMContentLoaded", function() {
     var title = document.getElementById("title");
     var from = document.getElementById("from");
     var to = document.getElementById("to");
 
+    var share_button = document.getElementById("send")
+
     const urlParams = new URLSearchParams(window.location.search);
 
-    if (localStorage.getItem('audio_link') && localStorage.getItem('text')){
+    // Explicitly check if all required localStorage items are present
+    if (localStorage.getItem('sender') && localStorage.getItem('recipient') && localStorage.getItem('text') && localStorage.getItem('audio_link')) {
         sender = localStorage.getItem('sender');
         recipient = localStorage.getItem('recipient');
         text = localStorage.getItem('text');
         audio_link = localStorage.getItem('audio_link');
-    }
-    else{
+    } else {
         sender = urlParams.get('sender');
         recipient = urlParams.get('recipient');
         text = urlParams.get('text');
         audio_link = urlParams.get('audio_link');
+        share_button.style.visibility = 'none'
     }
 
     title.innerHTML = "Happy Birthday!";
     from.innerHTML = "From: " + sender;
-    splitString(text, 30); 
+    splitString(text, 30);
     to.innerHTML = "To: " + recipient;
 
     setAudio();
@@ -35,14 +37,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function splitString(stringToSplit, limit) {
     let message = [];
-    let container = document.getElementById("message"); // Corrected ID
+    let container = document.getElementById("message");
 
     for (var i = 0; i < stringToSplit.length; i += 1) {
         if (i % limit === 0 && i !== 0) {
             var newLine = document.createElement("p");
             newLine.innerHTML = message.join('');
             container.appendChild(newLine);
-            message = []; 
+            message = [];
         }
         message.push(stringToSplit[i]);
     }
@@ -65,14 +67,12 @@ function share() {
 }
 
 function setAudio() {
-
     let audio_player = document.getElementById('audio-player');
     let audioChunks = sessionStorage.getItem('audio_chunks');
 
-    if (audio_link){
+    if (audio_link) {
         audio_player.src = audio_link;
-    }
-    else if (audioChunks) {
+    } else if (audioChunks) {
         try {
             let base64Chunks = JSON.parse(audioChunks);
             if (base64Chunks && Array.isArray(base64Chunks)) {
@@ -94,8 +94,7 @@ function setAudio() {
         } catch (error) {
             console.error("Error parsing JSON: ", error);
         }
-    }
-    else { 
-        alert("No Audio available")
+    } else {
+        alert("No Audio available");
     }
 }
