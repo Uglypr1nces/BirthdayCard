dde58493766913062af8dba77233271cc6b56607
Uglypr1nces <shpatavdiu4@gmail.com>
2025-01-16 19:40:58 +0100

    changed some stuff

--- form/script.js
+++ form/script.js
@@ -1,3 +1,6 @@
+let audio_link;
+let audio_chunks;
+
 document.addEventListener('DOMContentLoaded', function() {
     var next_button = document.getElementById('next-button');
     next_button.style.display = 'none';
@@ -8,28 +11,29 @@ document.getElementById('next-button').onclick = function() {
     var sender = document.getElementById('name').value;
     var recipient = document.getElementById('receiver_name').value;
     var text = document.getElementById('text').value;
-    var audio_link = document.getElementById('audio-link').value
+    audio_link = document.getElementById('audio-link').value
 
     localStorage['sender'] = sender;
     localStorage['recipient'] = recipient;
     localStorage['text'] = text;
-    localStorage['audio_link'] = audio_link
+    localStorage['audio_link'] = audio_link;
+    localStorage['audio_chunks'] = audio_chunks;
 }
 document.getElementById('check-button').onclick = function(event) {
     event.preventDefault(); // Prevent form submission
 
-    var audio_link = document.getElementById('audio-link').value;
-    var audio_player = document.getElementById('audio-player');
+    audio_link = document.getElementById('audio-link').value;
+    audio_chunks = document.getElementById('audio-player');
 
-    if (audio_link !== '' && audio_player.src) {
+    if (audio_link !== '' && audio_chunks.src) {
         alert('You cant add a link and an audio');
     }
-    else if (audio_link === '' && audio_player.src === '') {
+    else if (audio_link === '' && audio_chunks.src === '') {
         alert('Add an audio or record your own');
     }    
-    else if (audio_link !== '' && !audio_player.src) {
+    else if (audio_link !== '' && !audio_chunks.src) {
         try {
-            audio_player.src = audio_link;
+            document.getElementById('audio-player').src = audio_link;
             var check_button = document.getElementById('check-button');
             var next_button = document.getElementById('next-button');
             check_button.style.display = 'none';
